bc8bc3397f369dda47ce5b693d66337d8672d4a7
hyk <hk@qq.com>
2019-07-15 20:34:53 +0800

    validate

---
+++ src/api/ajax.js
@@ -0,0 +1,7 @@
+//封装的能发ajax请求函数
+import axios from 'axios'
+
+
+
+
+export default axios
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Route,Switch,BrowserRouter} from 'react-router-dom'
 import ReactDOM from 'react-dom';
 import App from './App'
+import './api'
 
 ReactDOM.render(
     <BrowserRouter> <App/> </BrowserRouter>
